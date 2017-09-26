01c145e24e11b7516fc08b8db2345acb3667dbad
AST <21300423@handong.edu>
2017-09-26 12:33:38 +0900

    test

---
+++ src/pages/park-list/park-list.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+import { NavController, NavParams } from 'ionic-angular';
+import {ParkData} from '../../app/providers/park-data';
+
+/*
+  Generated class for the ParkList page.
+
+  See http://ionicframework.com/docs/v2/components/#navigation for more info on
+  Ionic pages and navigation.
+*/
+@Component({
+  selector: 'page-park-list',
+  templateUrl: 'park-list.html'
+})
+export class ParkListPage {
+  parks: Array<Object> = [];
+  constructor(public navCtrl: NavController, public parkData: ParkData) {
+    parkData.getParks().then(theResult => {
+      this.parks = theResult;
+    })
+  }
+  goParkDetails(theParkData){
+    console.log(theParkData);
+  }
+
+}
